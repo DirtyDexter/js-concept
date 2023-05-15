39c93f3e8f53d4db7ed815c97ad1bd4b15e192ec
RAHUL KUMAR <49387348+DirtyDexter@users.noreply.github.com>
2023-05-15 13:31:36 +0530

    Added new variations of curry function

--- curry.js
+++ curry.js
@@ -9,3 +9,47 @@ function curry(fn) {
   }
   return nest(fn.length, []);
 }
+
+
+var curry = function(fn) {
+   return function curried(...args) {
+      if(args.length >= fn.length) {
+         return fn(...args);
+      }
+
+      return (...nextArgs) => curried(...args, ...nextArgs);
+   };
+};
+
+var curry = function (fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    }
+
+    return curried.bind(this, ...args);
+  };
+};
+
+/* Another popular variation is a curry function that doesn't accept a predefined amount of arguments 
+    (the function doesn't have a predefined length e.g const getSum = (...args) => args.reduce((a, b) => a + b, 0)) 
+    and is called when the user doesn't pass any arguments.
+*/
+
+var curry = function (fn) {
+  return function curried(...args) {
+    if (args.length === 0) {
+      return fn(...args);
+    }
+
+    return (...nextArgs) => {
+      if (nextArgs.length === 0) {
+        return fn(...args);
+      }
+
+      return curried(...args, ...nextArgs);
+    };
+  };
+};
+
+

714971003b59bec36527192ccaf772b53e886f91
Rahul Kumar <rkumar3@rkumar373M7L.vmware.com>
2023-05-19 22:29:10 +0530

    Added new variation of debounce

--- debounce.js
+++ debounce.js
@@ -7,3 +7,41 @@ const debounce = (func, delay) => {
     inDebounce = setTimeout(() => func.apply(context, args), delay)
   }
 }
+
+
+var debounce2 = function(fn, t) {
+  let timeout;
+  return function(...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      fn(...args)
+    }, t);
+  };
+};
+
+
+var debounce3 = function(fn, t) {
+  let interval;
+  return function(...args) {
+    const lastCall = Date.now()
+    clearInterval(interval);
+    interval = setInterval(() => {
+      if (Date.now() - lastCall >= t) {
+        fn(...args);
+        clearInterval(interval);
+      }
+    }, 1);
+  }
+};
+
+var debounce4 = function(fn, t) {
+  var timer;
+  return function(...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+        fn(...args);
+        clearTimeout(timer);
+    }, t);
+  }
+};
+

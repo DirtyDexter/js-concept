a20eb3188721b151f077ab63a8c8f201da415f2a
Rahul Kumar <rkumar3@rkumar373M7L.vmware.com>
2023-05-21 22:53:18 +0530

    Added more variants of throttle

--- throttle-advance.js
+++ throttle-advance.js
@@ -18,3 +18,89 @@ const throttle = (func, limit) => {
     }
   }
 }
+
+
+var throttle2 = function(fn, t) {
+  var prevArg;
+  var timer;
+  return function helper(...args) {
+      if (timer) {
+          prevArg = args;
+      } else {
+          fn(...args);
+          prevArg = null;
+          timer = setTimeout(() => {
+              //clearTimeout(timer);
+              timer = undefined;
+              if (prevArg) {
+                  return helper(...prevArg);
+              }
+          }, t);
+      }
+  }
+};
+
+
+
+var throttle3 = function(fn, t) {
+  let timeoutInProgress = null;
+  let argsToProcess = null;
+  
+  const timeoutFunction = () => {
+    if (argsToProcess === null) {
+      timeoutInProgress = null; // enter the waiting phase
+    } else {
+      fn(...argsToProcess);
+      argsToProcess = null;
+      timeoutInProgress = setTimeout(timeoutFunction, t);
+    }
+  };
+
+  return function throttled(...args) {
+    if (timeoutInProgress) {
+      argsToProcess = args;
+    } else {
+      fn(...args); // enter the looping phase
+      timeoutInProgress = setTimeout(timeoutFunction, t);
+    }
+  }
+};
+
+
+var throttle4 = function(fn, t) {
+  let intervalInProgress = null;
+  let argsToProcess = null;
+  
+  const intervalFunction = () => {
+    if (argsToProcess === null) {
+      clearInterval(intervalInProgress);
+      intervalInProgress = null; // enter the waiting phase
+    } else {
+      fn(...argsToProcess);
+      argsToProcess = null;
+    }
+  };
+
+  return function throttled(...args) {
+    if (intervalInProgress) {
+      argsToProcess = args;
+    } else {
+      fn(...args); // enter the looping phase
+      intervalInProgress = setInterval(intervalFunction, t);
+    }
+  }
+};
+
+
+var throttle5 = function(fn, t) {
+  let timeout = null;
+  let nextTimeToCallFn = 0;
+  return function(...args) {
+    const delay = Math.max(0, nextTimeToCallFn - Date.now());
+    clearTimeout(timeout);
+    timeout = setTimeout(() => { 
+      fn(...args);
+      nextTimeToCallFn = Date.now() + t;
+    }, delay);
+  }
+};
